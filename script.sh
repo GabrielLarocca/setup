38535d309e24642c04af2eefa8acf13b1baf88bb
Gabriel Larocca <glarocca@laon.com.br>
2021-04-28 16:07:54 -0300

    att

--- script.sh
+++ script.sh
@@ -18,6 +18,7 @@ sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
 sudo apt-get update
+sudo apt-get upgrade -y
 
 clear
 
